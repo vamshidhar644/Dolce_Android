f78ba36c42baccfa3606d1de294c9c3a1a51b448
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2022-11-21 21:17:34 +0530

    Second

---
+++ app/src/main/java/com/example/dolce_v3/Activities/CategoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.dolce_v3.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dolce_v3.R;
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
