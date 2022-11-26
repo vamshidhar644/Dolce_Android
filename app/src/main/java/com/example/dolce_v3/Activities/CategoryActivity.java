6542a95109e6fa5a3d416c1bde83f610058175f2
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2022-11-26 13:52:55 +0530

    Category items classified successfully

--- app/src/main/java/com/example/dolce_v3/Activities/CategoryActivity.java
+++ app/src/main/java/com/example/dolce_v3/Activities/CategoryActivity.java
@@ -34,28 +34,44 @@ public class CategoryActivity extends AppCompatActivity {
 
         TextView CategoryTitle;
         CategoryTitle = findViewById(R.id.CategoryTitle);
-
         object = (CategoryDomain) getIntent().getSerializableExtra("object");
         CategoryTitle.setText(object.getTitle()+" Ice creams");
 
+        String stick = "Stick Ice creams";
+        String cone = "Cone Ice creams";
+
+        if(CategoryTitle.getText().equals(cone)){
+            category_1();
+        }
+        else if(CategoryTitle.getText().equals(stick)){
+            category_2();
+        }
+    }
+
+    private void category_1(){
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
         recyclerViewCategoriesList = findViewById(R.id.ListCategory);
         recyclerViewCategoriesList.setLayoutManager(gridLayoutManager);
 
-        category_1();
-    }
+        ArrayList<PopularDomain> cat1_List = new ArrayList<>();
+        cat1_List.add(new PopularDomain("Belgian Chocolate", "belgian", "Bite into a rich, crunchy and chocolatey experience.", 45.00));
+        cat1_List.add(new PopularDomain("Chocolate Truffle", "choclate", "A divine combination of two flavours, guaranteed to delight you.", 40.00));
 
-    private void category_1(){
-        ArrayList<CategoryListDomain> cat1_List = new ArrayList<>();
-        cat1_List.add(new CategoryListDomain("Big - Belgian Chocolate", "belgian", "Bite into a rich, crunchy and chocolatey experience.", 45.00));
-        cat1_List.add(new CategoryListDomain("Raspberry Chocolate Truffle", "choclate", "A divine combination of two flavours, guaranteed to delight you.", 40.00));
-
-        categoryListAdaptor = new CategoryListAdaptor(cat1_List);
+        categoryListAdaptor = new PopularAdaptor(cat1_List);
         recyclerViewCategoriesList.setAdapter(categoryListAdaptor);
     }
 
 
     private void category_2(){
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        recyclerViewCategoriesList = findViewById(R.id.ListCategory);
+        recyclerViewCategoriesList.setLayoutManager(gridLayoutManager);
 
+        ArrayList<PopularDomain> cat1_List = new ArrayList<>();
+        cat1_List.add(new PopularDomain("Frosticks", "frostick", "A choclate centre wrapped in real milk choclate ice cream.", 15.00));
+        cat1_List.add(new PopularDomain("Probiotic (20)", "probiotic", "20 Sticks of 60ml each. Tastier and healthier than ice cream. Amul Pro-biotic Chocobar comes with the goodness of probiotic bacteria, real milk and choclate. ", 300.00));
+
+        categoryListAdaptor = new PopularAdaptor(cat1_List);
+        recyclerViewCategoriesList.setAdapter(categoryListAdaptor);
     }
 }
